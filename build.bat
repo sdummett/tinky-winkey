3a43c2d48a9ce2a3d0412a3be6b0e73b4f9f33fa
Stone <sdummett.dev@proton.me>
2025-02-16 18:09:44 +0100

    Add build script that calls the service & keylogger Makefiles

---
+++ build.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+echo ===========================
+echo ==== Building Keylogger ===
+echo ===========================
+cd keylogger
+nmake
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to build keylogger.
+    exit /b %ERRORLEVEL%
+)
+copy /Y keylogger.exe ..\
+cd ..
+
+echo ===========================
+echo ==== Building Service =====
+echo ===========================
+cd service
+nmake
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to build service.
+    exit /b %ERRORLEVEL%
+)
+copy /Y service.exe ..\
+cd ..
+
+echo ===========================
+echo ==== Build Successful! ====
+echo ===========================
+exit /b 0
